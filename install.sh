f2db57ebcaa1609533c53a2a81f8ba8e9fb29923
Adrian Vasile <adrian.vasile@sparktech.ro>
2011-09-27 17:03:49 +0300

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir ~/.vim-tmp
 ln -s ~/vim_config ~/.vim
-echo "source ~/vim_config/gvimrc" >> ~/.gvimrc
+echo "source ~/vim_config/vimrc" >> ~/.gvimrc
+echo "source ~/vim_config/vimrc" >> ~/.vimrc
 
